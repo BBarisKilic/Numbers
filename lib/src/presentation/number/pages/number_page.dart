600412a58fae1a61270b98942c1a8f63bf55d3c7
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-06-22 22:11:01 +0300

    Updated NumberPage.

--- lib/src/presentation/number/pages/number_page.dart
+++ lib/src/presentation/number/pages/number_page.dart
@@ -8,21 +8,36 @@ class NumberPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(title: const Text('Number')),
+        appBar: AppBar(
+          title: const Text(
+            'Numbers',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          bottom: const PreferredSize(
+            preferredSize: Size.fromHeight(1.0),
+            child: Divider(thickness: 1.0, height: 0.0),
+          ),
+        ),
         body: Center(
           child: Column(
             children: [
-              TextButton(
+              OutlinedButton(
                 onPressed: () =>
                     context.read<NumberCubit>().getNumberInfo(number: 5),
-                child: const Text('Get Info'),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Text('Get Info'),
+                ),
               ),
               BlocBuilder<NumberCubit, NumberState>(
-                builder: (context, state) => state.when(
+                builder: (_, state) => state.when(
                   initial: () => const SizedBox(),
                   loading: () => const CircularProgressIndicator(),
-                  loaded: (info) => Text(info),
+                  loaded: Text.new,
                   error: () => const SizedBox(),
                 ),
               ),
