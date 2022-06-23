f458970e4b1b3e15808c3196cd99a8e9fee2b473
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-06-23 21:16:09 +0300

    Created InfoContainer widget.

---
+++ lib/src/presentation/number/widgets/info_container.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sizer/sizer.dart';
+
+import '../../../core/core.dart';
+import '../number.dart';
+
+class InfoContainer extends StatelessWidget {
+  const InfoContainer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Container(
+        height: 28.h,
+        padding: const EdgeInsets.symmetric(
+          vertical: kVerticalSpace,
+          horizontal: kHorizontalSpace,
+        ),
+        decoration: BoxDecoration(
+          border: Border.all(color: Theme.of(context).dividerColor),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: BlocBuilder<NumberCubit, NumberState>(
+              builder: (_, state) => state.when(
+                initial: () => const SizedBox(),
+                loading: () => const CircularProgressIndicator(),
+                loaded: (info) => Text(
+                  info,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                error: () => const SizedBox(),
+              ),
+            ),
+          ),
+        ),
+      );
+}
