7075d8e5d01547eebf7a04c4b36a4a3dbf9d27dc
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-06-23 12:09:42 +0300

    Added new contants.

--- lib/src/core/utils/constants.dart
+++ lib/src/core/utils/constants.dart
@@ -1,3 +1,13 @@
 const String kMaterialAppTitle = 'Numbers';
 
 const String kBaseUrl = 'http://numbersapi.com';
+
+const String kLightTheme = 'light_theme';
+const String kDarkTheme = 'dark_theme';
+
+const double kHorizontalSpace = 22.0;
+const double kVerticalSpace = 12.0;
+const double kLongVerticalSpace = 22.0;
+const double kVeryLongVerticalSpace = 44.0;
+const double kThickStrokeWidth = 2.0;
+const double kThinStrokeWidth = 1.0;
