b2710a6485190a1efd92835ff07108dd0df765b3
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-06-26 17:41:34 +0300

    Added usecase dependencies to AppCubit.

--- lib/src/app/cubit/app_cubit.dart
+++ lib/src/app/cubit/app_cubit.dart
@@ -1,15 +1,54 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+import '../../core/core.dart';
+import '../../domain/domain.dart';
+import '../../domain/usecases/set_string_usecase.dart';
+
 part 'app_state.dart';
 part 'app_cubit.freezed.dart';
 
 class AppCubit extends Cubit<AppState> {
-  AppCubit() : super(const AppState(theme: AvailableTheme.dark));
+  final GetStringUseCase _getStringUseCase;
+  final SetStringUseCase _setStringUseCase;
+
+  AppCubit({
+    required GetStringUseCase getStringUseCase,
+    required SetStringUseCase setStringUseCase,
+  })  : _getStringUseCase = getStringUseCase,
+        _setStringUseCase = setStringUseCase,
+        super(const AppState(theme: AvailableTheme.dark)) {
+    _getAppThemeFromSharedPref();
+  }
+
+  Future<void> _getAppThemeFromSharedPref() async {
+    final dataState = await _getStringUseCase(
+      params: SharedPrefStringRequestParams(key: '$AvailableTheme'),
+    );
+
+    if (dataState is DataFailure) return;
+
+    emit(
+      state.copyWith(
+        theme: dataState.data == '${AvailableTheme.light}'
+            ? AvailableTheme.light
+            : AvailableTheme.dark,
+      ),
+    );
+  }
+
+  Future<void> _saveAppThemeToSharedPref(AvailableTheme theme) async =>
+      await _setStringUseCase(
+        params: SharedPrefSetStringParams(
+          key: '$AvailableTheme',
+          value: '$theme',
+        ),
+      );
 
   void updateTheme(AvailableTheme theme) {
     if (state.theme == theme) return;
 
+    _saveAppThemeToSharedPref(theme);
     emit(state.copyWith(theme: theme));
   }
 }
