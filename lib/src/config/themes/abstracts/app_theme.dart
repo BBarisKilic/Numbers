aaab8c4de5690becb947564d2e785e08688c4dd4
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2023-10-01 23:53:16 +0200

    docs: add docs for the AppTheme and AvailableTheme

--- lib/src/config/themes/abstracts/app_theme.dart
+++ lib/src/config/themes/abstracts/app_theme.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 
+/// An enum that represents the available themes.
+///
+/// ```dart
+/// final theme = AvailableTheme.light;
+/// ```
 enum AvailableTheme {
+  /// The [light] theme.
   light,
+
+  /// The [dark] theme.
   dark;
 
+  /// Returns the name of the enum -> `AvailableTheme`.
   String get key => toString().split('.').first;
 
+  /// Returns the value of the enum. Possible values are `light` and `dark`.
   String get value => toString().split('.').last;
 }
 
+/// An abstract interface class that blueprints of the concrete theme
+/// implementations.
 abstract interface class AppTheme {
   ThemeData get getData;
 }
