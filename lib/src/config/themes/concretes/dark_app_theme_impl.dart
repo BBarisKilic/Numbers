52711eab582d279eb98748e2acd56026d4f680ca
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2023-10-02 00:41:14 +0200

    docs: add docs for the DarkAppThemeImpl

--- lib/src/config/themes/concretes/dark_app_theme_impl.dart
+++ lib/src/config/themes/concretes/dark_app_theme_impl.dart
@@ -2,34 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:numbers/src/config/config.dart';
 import 'package:sizer/sizer.dart';
 
+/// An implementation of the [AppTheme] interface class that provides the dark
+/// theme.
 class DarkAppThemeImpl implements AppTheme {
   @override
   ThemeData get getData => _data;
 
-  final ThemeData _data = ThemeData(
+  final _data = ThemeData(
     fontFamily: 'RobotoMono',
-    primaryColor: const Color(0xff181818),
-    primaryColorDark: const Color(0xff181818),
-    primaryColorLight: const Color(0xffF8F3F7),
-    scaffoldBackgroundColor: const Color(0xff181818),
-    cardColor: const Color(0xff181818),
-    indicatorColor: const Color(0xb3F8F3F7),
+    primaryColor: const Color(0xFF181818),
+    primaryColorDark: const Color(0xFF181818),
+    primaryColorLight: const Color(0xFFF8F3F7),
+    scaffoldBackgroundColor: const Color(0xFF181818),
+    cardColor: const Color(0xFF181818),
+    indicatorColor: const Color(0xB3F8F3F7),
     focusColor: const Color(0x66181818),
     dividerColor: const Color(0x33F8F3F7),
     splashColor: const Color(0x33F8F3F7),
-    textSelectionTheme:
-        const TextSelectionThemeData(cursorColor: Color(0xffF8F3F7)),
-    progressIndicatorTheme: const ProgressIndicatorThemeData(
-      color: Color(0xffF8F3F7),
+    textSelectionTheme: const TextSelectionThemeData(
+      cursorColor: Color(0xFFF8F3F7),
     ),
-    iconTheme: const IconThemeData(color: Color(0xffF8F3F7)),
+    progressIndicatorTheme: const ProgressIndicatorThemeData(
+      color: Color(0xFFF8F3F7),
+    ),
+    iconTheme: const IconThemeData(
+      color: Color(0xFFF8F3F7),
+    ),
     appBarTheme: AppBarTheme(
-      color: const Color(0xff181818),
+      color: const Color(0xFF181818),
       elevation: 0,
       titleTextStyle: TextStyle(
         fontSize: 24.sp,
         fontWeight: FontWeight.w400,
-        color: const Color(0xffF8F3F7),
+        color: const Color(0xFFF8F3F7),
       ),
     ),
     textTheme: TextTheme(
@@ -62,14 +67,16 @@ class DarkAppThemeImpl implements AppTheme {
         fontWeight: FontWeight.w400,
       ),
     ).apply(
-      bodyColor: const Color(0xffF8F3F7),
-      displayColor: const Color(0xffF8F3F7),
+      bodyColor: const Color(0xFFF8F3F7),
+      displayColor: const Color(0xFFF8F3F7),
     ),
     colorScheme: const ColorScheme.dark()
         .copyWith(
-          primary: const Color(0xff838383),
-          secondary: const Color(0xffFF9E0B),
+          primary: const Color(0xFF838383),
+          secondary: const Color(0xFFFF9E0B),
         )
-        .copyWith(background: const Color(0xff181818)),
+        .copyWith(
+          background: const Color(0xFF181818),
+        ),
   );
 }
