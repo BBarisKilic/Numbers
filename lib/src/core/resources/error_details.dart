4e04c67b4b52bf6bc9b1c934af0aeeeca69073ef
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2023-10-09 00:24:32 +0200

    refactor: update ErrorDetails class

--- lib/src/core/resources/error_details.dart
+++ lib/src/core/resources/error_details.dart
@@ -1,11 +1,11 @@
 final class ErrorDetails {
   const ErrorDetails({
-    required this.message,
     required this.error,
+    required this.message,
     required this.stackTrace,
   });
 
-  final String message;
   final String error;
+  final String message;
   final String stackTrace;
 }
