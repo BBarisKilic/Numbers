e3eeec8c218ea9345f297b882853538e386a7b9a
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2023-10-16 21:19:51 +0200

    refactor: make MockNumberRepository class final

--- test/helpers/mocks/mock_number_repository.dart
+++ test/helpers/mocks/mock_number_repository.dart
@@ -1,4 +1,4 @@
 import 'package:mocktail/mocktail.dart';
 import 'package:numbers/src/features/number/number.dart';
 
-class MockNumberRepository extends Mock implements NumberRepository {}
+final class MockNumberRepository extends Mock implements NumberRepository {}
