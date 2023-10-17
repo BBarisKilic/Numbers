4118f15dd1cb642fce17cea0cd343a3a46766316
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2023-10-18 00:51:05 +0200

    feat: create MockGetRandomNumberUseCase helper class for tests

---
+++ test/helpers/mocks/mock_get_random_number_use_case.dart
@@ -0,0 +1,5 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:numbers/src/features/number/domain/usecases/get_random_number_usecase.dart';
+
+final class MockGetRandomNumberUseCase extends Mock
+    with MockGetRandomNumberUseCaseMixin {}
