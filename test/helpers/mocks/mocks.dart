444e351d10d600a51055d2bb37b3aec661947bd2
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2023-10-18 00:51:24 +0200

    test: add tests for NumberCubit

--- test/helpers/mocks/mocks.dart
+++ test/helpers/mocks/mocks.dart
@@ -1,3 +1,5 @@
 export 'mock_dio.dart';
+export 'mock_get_number_use_case.dart';
+export 'mock_get_random_number_use_case.dart';
 export 'mock_numbers_repository.dart';
 export 'mock_numbers_service.dart';
